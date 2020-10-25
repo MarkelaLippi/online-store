0e0f99979d6f6fe625385666b1864b9545f2c242
markelalippi <markelalippi@gmail.com>
2020-10-25 20:42:30 +0300

    Set build.gradle files for project and subprojects

--- configuration/src/test/java/com/gmail/roadtojob2019/configuration/OnlineStoreApplicationTests.java
+++ configuration/src/test/java/com/gmail/roadtojob2019/configuration/OnlineStoreApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gmail.roadtojob2019.controller;
+package com.gmail.roadtojob2019.configuration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

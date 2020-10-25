4bfa8db98866d1d8e72c170cb48558bf74e4e7ed
markelalippi <markelalippi@gmail.com>
2020-10-25 16:11:15 +0300

    Create structure of multimodule project

---
+++ controller-module/src/main/java/com/gmail/roadtojob2019/controller/OnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.roadtojob2019.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreApplication.class, args);
+	}
+
+}

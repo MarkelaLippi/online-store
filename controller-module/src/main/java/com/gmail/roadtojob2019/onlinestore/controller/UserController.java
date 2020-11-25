55bcf975d814d8ef7deb1253fab985e735550274
markelalippi <markelalippi@gmail.com>
2020-11-25 20:25:24 +0300

    Add pagination to page users

--- controller-module/src/main/java/com/gmail/roadtojob2019/onlinestore/controller/UserController.java
+++ controller-module/src/main/java/com/gmail/roadtojob2019/onlinestore/controller/UserController.java
@@ -19,10 +19,11 @@ public class UserController {
     @GetMapping("/users")
     @ResponseStatus(HttpStatus.OK)
     String getPageOfUsersSortedByEmail(Model model,
-                                       @RequestParam(required = false, defaultValue = "0") int pageNumber,
-                                       @RequestParam(required = false, defaultValue = "10") int pageSize) {
+                                       @RequestParam(name = "number", required = false, defaultValue = "0") int pageNumber,
+                                       @RequestParam(name = "size", required = false, defaultValue = "3") int pageSize) {
         final UsersPageDto pageOfUsers = userService.getPageOfUsersSortedByEmail(pageNumber, pageSize);
         model.addAttribute("users", pageOfUsers.getUsers());
+        model.addAttribute("pages", pageOfUsers.getTotalNumberOfPages());
         return "users";
     }
 }
--- repository-module/src/main/java/com/gmail/roadtojob2019/onlinestore/repository/entity/User.java
+++ repository-module/src/main/java/com/gmail/roadtojob2019/onlinestore/repository/entity/User.java
@@ -1,10 +1,7 @@
 package com.gmail.roadtojob2019.onlinestore.repository.entity;
 
 import com.gmail.roadtojob2019.onlinestore.repository.converter.RoleConverter;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -13,13 +10,13 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
+@Table(name = "USERS")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(value=AccessLevel.NONE)
     private Long id;
     private LastMiddleFirstName lastMiddleFirstName;
-    @Column(name = "email")
     private String email;
     @Column(name = "user_role")
     @Convert(converter = RoleConverter.class)
